aaeedf002da4ccbf5f2363632d9272718aed48d7
Hannah Freudenberger <hlf0329@cmpt251.westminstercollege.edu>
2019-11-07 13:18:53 -0700

    initializing proj to git

---
+++ proj/command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function help() {
+	echo "list- list the uncompleted items with numbers"
+	echo "complete number- mark the tiem with the given number as completed"
+	echo "list completed- list the completed items"
+	echo "add title- adds a new item with the given title"
+}
+
+function list() {
+	echo "list of all uncompleted tasks"
+}
+
+#help
+#list
+
+
+
+
