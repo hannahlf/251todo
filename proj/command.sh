94e8ed06942a1a3be075feafcb659bcd201ea532
Hannah Freudenberger <hlf0329@cmpt251.westminstercollege.edu>
2019-11-14 22:21:42 -0700

    first submission few minor bugs that need fixing

--- proj/command.sh
+++ proj/command.sh
@@ -12,116 +12,209 @@ function add(){
 	#add new item to todo
 	#my cntr is not working to name and create titles 	
 	read contents
+
 	for file in todo/*; do
+		cntr=$(ls todo +  ls completed | wc -l)
+		echo $cntr
 		if [ -f ${filename} ]; then
-			cntr=0
-			cntr=$(($cntr+1))
+			echo "This file already exists"	
 		fi
+		echo $cntr
+		cntr=$((cntr+1))	
 	done
 	filename=$cntr	
 	title=$"$1"
 	touch ${filename}.txt
-	echo $title >> ${filename}.txt
+	echo $1 >> ${filename}.txt
 	echo $contents >> ${filename}.txt
+	echo $2 >> ${filename}.txt
 	#chmod 700 ${filename}
  	mv ${filename}.txt todo
+	echo "item added to list!"
+	echo "-----------------------------------"
+	echo ""
 
 }
 
 function help() {
 	#lists all the functions that can be run
+	echo ""
+	echo "This is a list of all the commands you can use"
+	echo ""
+	echo "help- pulls up a list of all valid commands"
 	echo "list- list the uncompleted items with numbers"
-	echo "complete number- mark the tiem with the given number as completed"
+	echo "complete (number) - mark the tiem with the given number as completed"
 	echo "list completed- list the completed items"
-	echo "add title- adds a new item with the given title"
+	echo "add (title) - adds a new item with the given title"
+	echo ""
+	echo "--------------------------"
+	echo ""
 }
 
 function list() {
 	cntr=0
 if [ "$1" == "completed" ]; then
 		echo "list of all completed items"
+		echo ""
 		for file in completed/*; do
 			if [ -f ${filename} ]; then
 				cntr=$(($cntr+1))
 				echo $cntr
-				echo $file
+				head -1 $file
 			fi
 		done
 else
 	echo "list of all uncompleted tasks"
+	echo ""
 	#cntr=0
-	
-	for file in todo/*; do
-		if [ -f ${filename} ]; then
-			cntr=$(($cntr+1))
-			echo $cntr
-			echo $file
-		fi
-	done
+	items=$(ls todo | wc -l) 
+	if [ $items != 0 ]; then
+		for file in todo/*; do
+			if [ -f ${filename} ]; then
+				cntr=$(($cntr+1))
+				echo $cntr
+				head -1 $file
+				fi
+
+			done
+	else
+		echo "There are no items on your todo list"
+		echo "--------------------------------------"
+		echo ""
+
+	fi
 fi
+echo ""
 }
 
 function listCompleted() {
 	echo "list of all completed tasks"
+	echo ""
 	cntr=0
 	for file in completed/*; do
 		if [ -f ${filename} ]; then
 			cntr=$(($cntr+1))
 			echo $cntr
-			echo ${file}
+			head -1 ${file}
 		fi
 	done
+	echo "------------------------"
+	echo ""
 }
 
 function complete(){
+
 	echo $1
 	if [ -f todo/$1 ]; then
 		echo "File found"
+		mv  todo/$1 completed
+		echo "File moved to completed"
+		echo "-------------------------"
+		echo ""
+	else 
+		echo "File not found"
 	fi
-#	for file in todo/*; do
-#		echo $file
-#		if [ -f $file ]; then
-#			mv "$file" ../completed
-#		else
-#			echo "File does not exist"
-#		fi
-#	done	
 }
 
 function menu() {
+	cntr=1
+	echo "Welcome to to-do list manager! Here are your current items: "
+	echo "list of all uncompleted tasks"
+	echo ""
+	items=$(ls todo | wc -l)
+	if [ $items != 0 ]; then
+		for file in todo/*; do
+			echo $cntr
+			head -1 $file
+			cntr=$((cntr+1))
+			
+		done
+	else
+		echo "There are no items on your todo list"
+		echo "--------------------------------------"
+		echo ""
 
-	echo "Welcome to to-do lsit manager! Here are your current items: "
-	list
+	fi
+
 	echo ""
 	echo "What would you like to do?"
+	echo "-----------------------------"
 	echo "Enter the number of a task to see more information on it"
-	echo "A) Mark an item as completed?"
+        echo "------------------------------"	
+	echo "A) Mark an item as completed"
 	echo "B) Add a new item"
 	echo "C) See completed items"
+	echo ""
+	echo "Q) Quit and use command line arguments"
 
-	echo "Q) Quit"
+	read -p "Enter your choice: " i
+	#add info on how to show details of this item
+	#use cat on the file name for description
+	cntr=1	
+	for file in todo/*; do
+		
+		
+	if [ "$i" == "$cntr" ]; then
+		cat $file
+		cntr=$((cntr+1))
+	else
+		cntr=$((cntr+1))
+	
+	fi
 
-	read -p "Enter your choice: "i
-	if [ "$i" == "A" ]; then
-		complete
-	fi
-	if [ "$i" == "B" ]; then
-		add
-	fi
-	if [ "$i" == "C" ]; then
+	done
+
+		
+
+	case "$i" in
+		"A") 
+		read -p "What number task would you like completed? " completeNum
+		cntr=1	
+		for file in todo/*; do
+			if [ "$completeNum" == "$cntr" ]; then
+				echo $completeNum
+				mv  $file completed
+				echo "File moved to completed"
+				cntr=$((cntr+1))
+			else	
+				cntr=$((cntr+1))
+			fi
+
+		done	
+		;;
+	
+        	"B")
+		read -p "Enter a title for the new item:  " title 
+		read -p "Enter the details for the new item: " contents 
+		add "$title" "$contents"
+		;;
+                "C")		
 		listCompleted
-	fi
-
-
+  		;;
+	"Q" )
+		exit
+		;;
+	*)
+		echo "Please enter a valid command"
+		help
+				;;
+esac
+menu
 
 }
 
-if [ "$#" != "0" ]; then
+if [ "$#" == "0" ]; then
+	menu
+
+
+elif [ "$#" != "0" ]; then
 	"$1" "$2"
 else
 	echo "Please enter a valid command"
 	help
 fi
+
+
 
 
 
